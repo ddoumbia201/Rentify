2d8636ace82d17a4f5b1a3f34f01f2ff8ab93f95
Dienab <dienab.doumbia@student.unamur.be>
2026-07-29 18:16:13 +0200

    Create RegisterRequest.java
    added RegisterRequest to dto package

---
+++ src/main/java/com/project/locationbiens/dto/RegisterRequest.java
@@ -0,0 +1,26 @@
+package com.project.locationbiens.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "The first name cannot be empty")
+    private String firstName;
+
+    @NotBlank(message = "The last name cannot be empty")
+    private String lastName;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "The email cannot be empty")
+    private String email;
+
+    @NotBlank(message = "The password cannot be empty")
+    private String password;
+
+    @NotBlank(message = "The role cannot be empty")
+    private String role; // USER or ADMIN
+}
+
